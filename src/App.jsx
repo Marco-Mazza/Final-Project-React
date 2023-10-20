3517c867dcb83b98d638e55756484974517ea018
Marco Mazza <marcomazza2804@gmail.com>
2023-10-20 14:28:53 +0200

    inizializzazione progetto finale

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='mt-12 text-center'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem aut quibusdam recusandae hic deserunt fuga libero non aspernatur nisi voluptatum doloremque laboriosam incidunt, voluptas nemo atque sapiente molestias quod distinctio.</div>
+    </>
+  )
+}
+
+export default App
